f3df754c2e5db19663f1723ea6cb0f9fe76a19bf
Angela Muñoz <angemunoz24@alumnos.utalca.cl>
2025-09-06 23:27:45 -0400

    Archivo de Interfaces DAO

---
+++ SRC/Backend/interfaces/ChatbotDAO.js
@@ -0,0 +1,9 @@
+class ChatbotDAO {
+  async saveConversation(conversacion) { throw new Error('Método no implementado'); }
+  async getConversations(sessionId) { throw new Error('Método no implementado'); }
+  async getFAQs() { throw new Error('Método no implementado'); }
+  async addFAQ(faq) { throw new Error('Método no implementado'); }
+  async incrementFAQCount(faqId) { throw new Error('Método no implementado'); }
+}
+
+module.exports = ChatbotDAO;
---
+++ SRC/Backend/interfaces/ProductosDAO.js
@@ -0,0 +1,11 @@
+class ProductosDAO {
+  async getAll() { throw new Error('Método no implementado'); }
+  async getById(id) { throw new Error('Método no implementado'); }
+  async getByCategory(categoria) { throw new Error('Método no implementado'); }
+  async create(producto) { throw new Error('Método no implementado'); }
+  async update(id, updates) { throw new Error('Método no implementado'); }
+  async delete(id) { throw new Error('Método no implementado'); }
+  async updateStock(id, nuevoStock) { throw new Error('Método no implementado'); }
+}
+
+module.exports = ProductosDAO;
