77401a99aaf0718391ed7355ef7017bfca437912
patriciomunozzz <felipemo336@gmail.com>
2025-12-10 19:42:08 -0300

    Upload seed with new categorys and new products.

--- src/backend/scripts/seed.js
+++ src/backend/scripts/seed.js
@@ -25,7 +25,7 @@ async function main() {
       email: 'pepeacosta@example.com',
       phone: '+56912345678',
       address: 'Calle de Pepe Acosta',
-      password: '12345678',
+      password: 'USER1234',
       role: 'USER',
       active: true
     },
@@ -35,7 +35,7 @@ async function main() {
       email: 'laura.fernandez@example.com',
       phone: '+56987654321',
       address: 'Av. Los Olivos 123',
-      password: '12345678',
+      password: 'USER1234',
       role: 'USER',
       active: false
     },
@@ -45,7 +45,7 @@ async function main() {
       email: 'martin.rojas@example.com',
       phone: '+56923187458',
       address: 'Calle Central 45',
-      password: '12345678',
+      password: 'USER1234',
       role: 'USER',
       active: true
     }
@@ -70,7 +70,10 @@ async function main() {
   const categories = [
     { id: 'cmils0rgj0000uirk24jfq2j3', name: 'Helados' },
     { id: 'cmils0rgj0001uirk24jfq2j4', name: 'Bebidas' },
-    { id: 'cmils0rgj0002uirk24jfq2j5', name: 'Snacks' }
+    { id: 'cmils0rgj0002uirk24jfq2j5', name: 'Snacks' },
+    { id: 'cmj0kp8lk0000uixkndd6684f', name: 'Chocolates' },
+    { id: 'cmj0g2u9g0000bw1w9kudajy9', name: 'Alfajores' },
+    { id: 'cmj0ju7ug0000ui1kt43so0di', name: 'Caramelos' }
   ];
 
   for (const category of categories) {
@@ -102,7 +105,7 @@ async function main() {
       categoryId: 'cmils0rgj0001uirk24jfq2j4',
       description: 'Refresca tus momentos con el sabor clásico de Cola Cola.',
       image: 'cola_cola_fruna.png',
-      famous: true
+      famous: false
     },
     {
       id: 'cj2xk7s6y0006s0ezeou7fcwn',
@@ -115,13 +118,123 @@ async function main() {
       famous: false
     },
     {
-      id: 'cmj0gt2vy100ebw1wyh4h23w3',
-      name: 'Serrano Frutal 2Lt',
+      id: 'cmiyuwa7h0002uikw3ue1e770',
+      name: 'Tabletones Maní',
       price: 2490,
       stock: 25,
+      categoryId: 'cmj0kp8lk0000uixkndd6684f',
+      description: 'Tabletones de maní bañados en chocolate.',
+      image: 'tabletones.png',
+      famous: true
+    },
+    {
+      id: 'cmj0g2ua00002bw1wwup4l70i',
+      name: 'Alfajor Panchito',
+      price: 390,
+      stock: 350,
+      categoryId: 'cmj0g2u9g0000bw1w9kudajy9',
+      description: 'Alfajor tradicional bañado en chocolate.',
+      image: 'alfajor_panchito.png',
+      famous: false
+    },
+    {
+      id: 'cmj0glv630006bw1wdi16okec',
+      name: 'Chispote',
+      price: 490,
+      stock: 25,
+      categoryId: 'cmils0rgj0002uirk24jfq2j5',
+      description: 'Sufle sabor chocolate.',
+      image: 'chispote.png',
+      famous: false
+    },
+    {
+      id: 'cmj0gn8a80008bw1wukdeuumv',
+      name: 'Choco Cranch',
+      price: 790,
+      stock: 35,
+      categoryId: 'cmils0rgj0002uirk24jfq2j5',
+      description: 'Cereal de maíz con cobertura sabor a chocolate',
+      image: 'choco_cranch.png',
+      famous: true
+    },
+    {
+      id: 'cmj0gq8na000abw1wytw78i2y',
+      name: 'Doble Chirlito Chocolate',
+      price: 290,
+      stock: 45,
+      categoryId: 'cmils0rgj0002uirk24jfq2j5',
+      description: 'Sufle con relleno crema sabor chocolate',
+      image: 'doble_chirlito_chocolate.png',
+      famous: false
+    },
+    {
+      id: 'cmj0grkfp000cbw1w2zcnyv5l',
+      name: 'Fruna Frutal 500ml',
+      price: 590,
+      stock: 32,
       categoryId: 'cmils0rgj0001uirk24jfq2j4',
-      description: 'Bebida sabor frutal de 2 litros.',
+      description: 'Bebida sabor frutal de 500ml',
+      image: 'fruna_frutal_500ml.png',
+      famous: false
+    },
+    {
+      id: 'cmj0gt2vy000ebw1wyh4h23wc',
+      name: 'Serrano Frutal 2L',
+      price: 2490,
+      stock: 5,
+      categoryId: 'cmils0rgj0001uirk24jfq2j4',
+      description: 'Bebida sabor frutal de 2 litros',
       image: 'serrano_frutal_2l.png',
+      famous: false
+    },
+    {
+      id: 'cmj0gv7ay000ibw1w2z0fz18g',
+      name: 'Fruna Fru Cola 3L',
+      price: 2490,
+      stock: 50,
+      categoryId: 'cmils0rgj0001uirk24jfq2j4',
+      description: 'Bebida sabor fru cola de 3 litros',
+      image: 'fruna_fru_cola_3l.png',
+      famous: true
+    },
+    {
+      id: 'cmj0h3vtw000kbw1w165ltqqz',
+      name: 'Brazuka',
+      price: 290,
+      stock: 35,
+      categoryId: 'cmils0rgj0000uirk24jfq2j3',
+      description: 'Helado de agua sabor a manzana limón',
+      image: 'brazuka.png',
+      famous: true
+    },
+    {
+      id: 'cmj0h7gmm000mbw1wfrctzdk6',
+      name: 'Choco Fru',
+      price: 290,
+      stock: 50,
+      categoryId: 'cmils0rgj0000uirk24jfq2j3',
+      description: 'Helado de leche con cobertura sabor a crocante',
+      image: 'choco_fru.png',
+      famous: false
+    },
+    {
+      id: 'cmj0hbtra000obw1wterwvf8k',
+      name: 'Palito Vannichoc',
+      price: 290,
+      stock: 60,
+      categoryId: 'cmils0rgj0000uirk24jfq2j3',
+      description: 'Helado de leche sabor a vainilla y sabor chocolate',
+      image: 'palito_vannichoc.png',
+      famous: false
+    },
+    {
+      id: 'cmj0ju8qq0002ui1k298toldj',
+      name: 'Almendras Confitadas Serrano 500g',
+      price: 1490,
+      stock: 60,
+      categoryId: 'cmj0ju7ug0000ui1kt43so0di',
+      description: 'Caramelos duros sabor a fresa.',
+      image: 'almendras_confitadas_serrano_500g.png',
       famous: false
     }
   ];
